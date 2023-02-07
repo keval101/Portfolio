492ebc8ebad59a95bfba2b784eb0bca2737c3cc1
keval101 <kevalvadhiya101@gmail.com>
2023-02-07 09:28:18 +0530

    update the route URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: DefaultPageComponent
   },
   {
-    path: 'project-detail/:id',
+    path: 'projects/:id',
     component: ProjectDetailComponent
   },
 ];
